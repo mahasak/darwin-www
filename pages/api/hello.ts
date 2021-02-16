6c4b2d31204db302b4509169e913175bffab9ed2
Max Pijittum <mpijittum@fb.com>
2021-02-16 23:11:39 +0800

    add dotenv

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,10 +1,12 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
+import * as dotenv from "dotenv";
 
+dotenv.config();
 type Data = {
   name: string
 }
 
 export default (req: NextApiRequest, res: NextApiResponse<Data>) => {
-  res.status(200).json({ name: 'John Doe' })
+  res.status(200).json({ name: `John Doe-${process.env.TEST_KEY}` })
 }
