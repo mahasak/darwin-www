cf5ebd33a5615d321d1fe4c37084beb00a588230
Max Pijittum <mpijittum@fb.com>
2021-02-18 00:38:59 +0800

    Add login placeholder

--- components/LoginControl.tsx
+++ components/LoginControl.tsx
@@ -1,5 +1,7 @@
 import React, { ReactNode } from "react";
+
 import Link from "next/link";
+import { signIn, useSession } from "next-auth/client";
 
 type Props = {
     isActive: (pathname: string) => boolean
