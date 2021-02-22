6b879004d8abb9e7f0162854165215b678be3fab
Max Pijittum <mpijittum@fb.com>
2021-02-22 15:05:34 +0800

    Add drawer

--- components/TopBar.tsx
+++ components/TopBar.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Link   from 'next/link';
+import Link from 'next/link';
 import clsx from 'clsx';
 import PropTypes from 'prop-types';
 import {
@@ -9,28 +9,21 @@ import {
 } from '@material-ui/core';
 import Logo from './Logo';
 
-const useStyles = makeStyles(({
-  root: {},
-  toolbar: {
-    height: 64
-  }
-}));
-
-const TopBar = () => {
-  const classes = useStyles();
-
-  return (
-    <AppBar
-      className={clsx(classes.root)}
-      elevation={0}
-    >
-      <Toolbar className={classes.toolbar}>
-        <Link href="/">
-          <a><Logo /></a>
-        </Link>
-      </Toolbar>
-    </AppBar>
-  );
+type Props = {
+  className: string;
 };
+
+const TopBar: React.FC<Props> = (props: Props) => (
+  <AppBar position="fixed"
+    className={clsx(props.className)}
+    elevation={0}
+  >
+    <Toolbar>
+      <Link href="/">
+        <a><Logo /></a>
+      </Link>
+    </Toolbar>
+  </AppBar>
+);
 
 export default TopBar;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,26 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 import Layout from '../components/Layout'
+import Logo from '../components/Logo'
 import { GetStaticProps } from "next";
 import { signIn, signOut, useSession } from "next-auth/client";
 import prisma from '../lib/prisma'
 import TopBar from '../components/TopBar'
+import Drawer from '@material-ui/core/Drawer';
+import AppBar from '@material-ui/core/AppBar';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Toolbar from '@material-ui/core/Toolbar';
+import List from '@material-ui/core/List';
+import Typography from '@material-ui/core/Typography';
+import Divider from '@material-ui/core/Divider';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import InboxIcon from '@material-ui/icons/MoveToInbox';
+import MailIcon from '@material-ui/icons/Mail';
+import Link from 'next/link';
+import { createStyles, Theme, makeStyles } from '@material-ui/core/styles';
+
 export const getServerSideProps = async ({req}) => {
   const token = req.headers.AUTHORIZATION
   //const userId = await getUserId(token)
@@ -76,12 +92,70 @@ function HomeApp() {
     </div>
   )
 }
+const drawerWidth = 240;
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      display: 'flex',
+    },
+    appBar: {
+      zIndex: theme.zIndex.drawer + 1,
+    },
+    drawer: {
+      width: drawerWidth,
+      flexShrink: 0,
+    },
+    drawerPaper: {
+      width: drawerWidth,
+    },
+    drawerContainer: {
+      overflow: 'auto',
+    },
+    content: {
+      flexGrow: 1,
+      padding: theme.spacing(3),
+    },
+  }),
+);
 
 export default function Home() {
   const [session, loading] = useSession();
+  const classes = useStyles();
+
   return (
     <Layout>
-      <TopBar />
+      <AppBar position="fixed" className={classes.appBar}>
+        <Toolbar>
+        <Link href="/">
+            <a><Logo /></a>
+          </Link>
+        </Toolbar>
+      </AppBar>
+      <Drawer className={classes.drawer}
+        variant="permanent"
+      >
+        <Toolbar />
+        <div >
+          <List>
+            {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+              <ListItem button key={text}>
+                <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+                <ListItemText primary={text} />
+              </ListItem>
+            ))}
+          </List>
+          <Divider />
+          <List>
+            {['All mail', 'Trash', 'Spam'].map((text, index) => (
+              <ListItem button key={text}>
+                <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+                <ListItemText primary={text} />
+              </ListItem>
+            ))}
+          </List>
+        </div>
+      </Drawer>
       <HomeApp/>
     </Layout>
   )
