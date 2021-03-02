38748228b35bb308ce54aa8056daab656991c7c6
Max Pijittum <mpijittum@fb.com>
2021-03-02 23:10:33 +0800

    Enable React FastRefresh on Storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,10 +1,13 @@
 module.exports = {
   "stories": [
     "../stories/**/*.stories.mdx",
-    "../stories/**/*.stories.@(js|jsx|ts|tsx)"
+    "../stories/**/*.stories.@(js|jsx|ts|tsx)",
   ],
   "addons": [
     "@storybook/addon-links",
     "@storybook/addon-essentials"
-  ]
+  ],
+  reactOptions: {
+    fastRefresh: true,
+  }
 }
